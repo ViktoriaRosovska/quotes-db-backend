55b03fd32fcdc160645d09238177d2c003200a7c
Viktoriia Rosovska <torimeister@gmail.com>
2026-02-13 16:23:44 +0200

    modify db config

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,25 @@
-const { DB } = require('./config');
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(DB.NAME, DB.USER, DB.PASSWORD, {
-  dialect: DB.DIALECT,
-  host: DB.HOST,
-  port: DB.PORT,
-  logging: false, // Set to true to log SQL queries
-});
+let sequelize;
+
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+    logging: false,
+  });
+} else {
+  sequelize = new Sequelize(DB.NAME, DB.USER, DB.PASSWORD, {
+    dialect: DB.DIALECT,
+    host: DB.HOST,
+    port: DB.PORT,
+    logging: false,
+  });
+}
 
 module.exports = sequelize;
